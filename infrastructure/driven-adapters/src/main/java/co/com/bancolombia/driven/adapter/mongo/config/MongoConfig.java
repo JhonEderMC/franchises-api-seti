31e32f7846b7df19e77ea78457ae8f7d1d4f6c4d
jhonederMC <jhoneder@hotmail.com>
2025-06-23 20:20:52 -0500

    Refactor MongoConfig to use connection string with username, password, and cluster URL

--- infrastructure/driven-adapters/src/main/java/co/com/bancolombia/driven/adapter/mongo/config/MongoConfig.java
+++ infrastructure/driven-adapters/src/main/java/co/com/bancolombia/driven/adapter/mongo/config/MongoConfig.java
@@ -1,6 +1,7 @@
 package co.com.bancolombia.driven.adapter.mongo.config;
 
-import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
 import com.mongodb.reactivestreams.client.MongoClients;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -14,21 +15,36 @@ import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRep
 @EnableReactiveMongoRepositories(basePackages = "co.com.bancolombia.driven.adapter.mongo.repository")
 public class MongoConfig extends AbstractReactiveMongoConfiguration {
 
-    @Value("${spring.data.mongodb.uri}") //TODO: configurar yml
-    private String mongoUri;
-
     @Value("${spring.data.mongodb.database}")
-    private String databaseName;
+    private String database;
+
+    @Value("${spring.data.mongodb.username}")
+    private String username;
+
+    @Value("${spring.data.mongodb.password}")
+    private String password;
+
+    @Value("${spring.data.mongodb.cluster-url}")
+    private String clusterUrl;
 
     @Override
-    protected String getDatabaseName() {
-        return databaseName;
+    public com.mongodb.reactivestreams.client.MongoClient reactiveMongoClient() {
+        String connectionUri = String.format(
+                "mongodb+srv://%s:%s@%s/%s?retryWrites=true&w=majority",
+                username, password, clusterUrl, database
+        );
+
+        ConnectionString connectionString = new ConnectionString(connectionUri);
+        MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+
+        return MongoClients.create(mongoClientSettings);
     }
 
     @Override
-    @Bean
-    public MongoClient reactiveMongoClient() {
-        return MongoClients.create(mongoUri);
+    protected String getDatabaseName() {
+        return database;
     }
 
     @Bean
